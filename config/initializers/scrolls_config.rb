e353bfa8712de7f6fd1e48b7129680789db5a01a
Daniel Ruskin <daniel@coinbase.com>
2015-07-11 10:10:25 -0400

    Use pid instead of process_id in Scrolls (for consistency)

--- config/initializers/scrolls_config.rb
+++ config/initializers/scrolls_config.rb
@@ -1,4 +1,4 @@
 require 'scrolls'
 
 Scrolls.add_timestamp = true
-Scrolls.global_context(app: "banksclosedtoday", process_id: Process.pid)
+Scrolls.global_context(app: "banksclosedtoday", pid: Process.pid)
