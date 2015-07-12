391356f5b3ab04cb5a72ea9571fdfdaa29568ac8
Daniel Ruskin <daniel@coinbase.com>
2015-07-12 18:08:33 -0400

    Remove extraneous catch_and_handle_errors method
    It used to be called in more than one plance - but it's extraneous now.

--- app/services/keen_service.rb
+++ app/services/keen_service.rb
@@ -6,25 +6,29 @@ class KeenService
   # 2. request (keyword arg) - The Rack::Request object associated with the action.
   # 3. tracking_params (keyword arg) - Parameters to send to Keen (e.g. the action's details).
   def self.track_action(action_name, request: nil, tracking_params: {})
-    catch_and_handle_errors do
-      # Log
-      track(method: :track_action, status: :started, action_name: action_name, tracking_params: tracking_params)
+    # Log
+    track(method: :track_action, status: :started, action_name: action_name, tracking_params: tracking_params)
 
-      # Merge request params into tracking_params, if a request was provided.
-      if request
-        tracking_params = tracking_params.merge(generate_request_params(request))
-      end
-
-      # Schedule publishing if on production
-      # Async publishing is used to avoid slowing down requests
-      Keen.publish_async(action_name, tracking_params) if Rails.env.production?
-
-      # Log
-      track(method: :track_action, status: :completed, action_name: action_name, tracking_params: tracking_params)
-
-      # Return true for success
-      true
+    # Merge request params into tracking_params, if a request was provided.
+    if request
+      tracking_params = tracking_params.merge(generate_request_params(request))
     end
+
+    # Schedule publishing if on production
+    # Async publishing is used to avoid slowing down requests
+    Keen.publish_async(action_name, tracking_params) if Rails.env.production?
+
+    # Log
+    track(method: :track_action, status: :completed, action_name: action_name, tracking_params: tracking_params)
+
+    # Return true for success
+    true
+  rescue => e
+    # As Keen tracking is non-critical, just notify Rollbar and return if an exception occurs.
+    # It's not worth failing a request if a Keen request fails.
+    Rollbar.error(e)
+
+    false
   end
 
   # Helper method to generate Keen tracking params for a Rack::Request object.
@@ -42,23 +46,6 @@ class KeenService
 
     # Return generated params
     tracking_params
-  end
-
-  ###
-  # Helper method to run a block, then take the following actions on exceptions
-  # 1. Notify Rollbar
-  # 2. Track on Scrolls
-  # 3. Return false
-  # As Keen tracking is non-critical, the above error handling will ensure that requests
-  # are not interrupted due to errors.
-  def self.catch_and_handle_errors
-    yield
-  rescue => e
-    # Rollbar
-    Rollbar.error(e)
-
-    # Return
-    false
   end
 
   # Helper method to log requests to KeenService requests.
