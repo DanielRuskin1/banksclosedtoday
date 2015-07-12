ab057b65d5e97ab3937c154ecf2b2b6e3a580088
Daniel Ruskin <daniel@coinbase.com>
2015-07-12 17:32:00 -0400

    Change XX to OTHER for unsupported country in dropdown
    Avoid confusion with the different UserLocationService::HOSTIP_INVALID_COUNTRY_CODE

--- spec/features/country_selection_feature_spec.rb
+++ spec/features/country_selection_feature_spec.rb
@@ -37,64 +37,68 @@ describe 'country selection', type: :feature do
     expect_keen_call(:bank_status_check, params)
   end
 
-  it "should use the user's country param when provided and show correct messaging" do
-    # Go to normal day
-    Timecop.travel(Time.parse('January 5, 2015').in_time_zone('Eastern Time (US & Canada)'))
+  context 'when a user provides a country_code param' do
+    it 'should use the param when provided and show correct messaging' do
+      # Go to normal day
+      Timecop.travel(Time.parse('January 5, 2015').in_time_zone('Eastern Time (US & Canada)'))
 
-    # Go to page with country param
-    visit root_path(country_code: 'US')
+      # Go to page with country param
+      visit root_path(country_code: 'US')
 
-    # Make sure GEOIP was not attempted
-    expect_no_geoip
+      # Make sure GEOIP was not attempted
+      expect_no_geoip
 
-    # Make sure correct messaging is shown
-    expect_open_us_day
+      # Make sure correct messaging is shown
+      expect_open_us_day
 
-    # Make sure KeenService was called correctly
-    expect_bank_status_check_keen_call
-    expect_no_keen_call(:country_lookup_success)
-    expect_no_keen_call(:country_lookup_failed)
+      # Make sure KeenService was called correctly
+      expect_bank_status_check_keen_call
+      expect_no_keen_call(:country_lookup_success)
+      expect_no_keen_call(:country_lookup_failed)
+    end
+
+    it 'should not care about case' do
+      # Go to normal day
+      Timecop.travel(Time.parse('January 5, 2015').in_time_zone('Eastern Time (US & Canada)'))
+
+      # Go to page with country param
+      visit root_path(country_code: 'us')
+
+      # Make sure GEOIP was not attempted
+      expect_no_geoip
+
+      # Make sure correct messaging is shown
+      expect_open_us_day
+
+      # Make sure KeenService was called correctly
+      expect_bank_status_check_keen_call
+      expect_no_keen_call(:country_lookup_success)
+      expect_no_keen_call(:country_lookup_failed)
+    end
   end
 
-  it 'should not care about case' do
-    # Go to normal day
-    Timecop.travel(Time.parse('January 5, 2015').in_time_zone('Eastern Time (US & Canada)'))
+  context 'when a user does not provide a country_code param' do
+    it 'should use GEOIP lookup otherwise and show correct messaging' do
+      # Stub GEOIP
+      stub_geoip_lookup('US')
 
-    # Go to page with country param
-    visit root_path(country_code: 'us')
+      # Go to normal day
+      Timecop.travel(Time.parse('January 5, 2015').in_time_zone('Eastern Time (US & Canada)'))
 
-    # Make sure GEOIP was not attempted
-    expect_no_geoip
+      # Go to page without country param
+      visit root_path
 
-    # Make sure correct messaging is shown
-    expect_open_us_day
+      # Make sure GEOIP was attempted once
+      expect_geoip_once
 
-    # Make sure KeenService was called correctly
-    expect_bank_status_check_keen_call
-    expect_no_keen_call(:country_lookup_success)
-    expect_no_keen_call(:country_lookup_failed)
-  end
+      # Make sure correct messaging is shown (the same as above)
+      expect_open_us_day
 
-  it 'should use GEOIP lookup otherwise and show correct messaging' do
-    # Stub GEOIP
-    stub_geoip_lookup('US')
-
-    # Go to normal day
-    Timecop.travel(Time.parse('January 5, 2015').in_time_zone('Eastern Time (US & Canada)'))
-
-    # Go to page without country param
-    visit root_path
-
-    # Make sure GEOIP was attempted once
-    expect_geoip_once
-
-    # Make sure correct messaging is shown (the same as above)
-    expect_open_us_day
-
-    # Make sure KeenService was called correctly
-    expect_country_lookup_success_keen_call
-    expect_no_keen_call(:country_lookup_failed)
-    expect_bank_status_check_keen_call
+      # Make sure KeenService was called correctly
+      expect_country_lookup_success_keen_call
+      expect_no_keen_call(:country_lookup_failed)
+      expect_bank_status_check_keen_call
+    end
   end
 
   context 'when an unsupported country is found' do
@@ -171,6 +175,72 @@ describe 'country selection', type: :feature do
         expect_no_keen_call(:country_lookup_failed)
         expect_bank_status_check_keen_call(country_code: 'NL', error: :unsupported_country)
         expect_bank_status_check_keen_call(country_code: 'US')
+      end
+    end
+  end
+
+  context 'when an unsupported country is selected' do
+    context 'when Javascript is enabled', js: true do
+      it 'should ask the user to email for support, or provide a different country' do
+        # Stub GEOIP with non-existant country
+        stub_geoip_lookup('XX')
+
+        # Go to normal day
+        Timecop.travel(Time.parse('January 5, 2015').in_time_zone('Eastern Time (US & Canada)'))
+
+        # Go to page without country param
+        visit root_path
+
+        # Make sure GEOIP was attempted once
+        expect_geoip_once
+
+        # Make sure correct messaging is shown
+        expect_no_country_error
+
+        # No "Go" buttons should be displayed, as the user has JS
+        expect(page).to_not have_button('Go!')
+
+        # Try choosing an unsupported country
+        select('Other', from: 'country_code')
+
+        # GEOIP should not have been attempted again
+        # (e.g. and should still be at 1 call total)
+        expect_geoip_once
+
+        # Make sure correct messaging is shown
+        expect_unsupported_country_error
+      end
+    end
+
+    context 'when Javascript is not enabled' do
+      it 'should do the same thing, but with a manual form submit' do
+        # Stub GEOIP with non-existant country
+        stub_geoip_lookup('XX')
+
+        # Go to normal day
+        Timecop.travel(Time.parse('January 5, 2015').in_time_zone('Eastern Time (US & Canada)'))
+
+        # Go to page without country param
+        visit root_path
+
+        # Make sure GEOIP was attempted once
+        expect_geoip_once
+
+        # Make sure correct messaging is shown
+        expect_no_country_error
+
+        # Try choosing an unsupported country
+        select('Other', from: 'country_code')
+
+        # Manually submit form
+        click_button('Go!')
+
+        # GEOIP should not have been attempted again
+        # (e.g. and should still be at 1 call total)
+        expect_geoip_once
+
+        # Make sure correct messaging is shown
+        expect_unsupported_country_error
       end
     end
   end
