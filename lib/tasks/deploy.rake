0eefae9b43eadf538e515e084e4f42cd98c26b5f
Daniel Ruskin <daniel@coinbase.com>
2015-07-19 16:03:37 -0400

    Improving deploy script
    1. Adding check for uncommited changes
    2. Moving existing code into methods
    3. More tests

--- lib/tasks/deploy.rake
+++ lib/tasks/deploy.rake
@@ -2,31 +2,58 @@ TESTS_FAILED_REGEX = /, [^0].* failure/ # If this matches, the task will assume 
 TESTS_PASSED_REGEX = /0 failures/
 ACCEPT_DEPLOY_TEXT = 'DEPLOY' # Text user has to enter to accept the deploy
 
+# Error class for exceptions that can occur during a deploy attempt
+class DeployError < StandardError; end
+
 task :deploy do
+  # Check for uncommited changes
+  check_for_uncommited_changes
+
+  # Run/verify tests
+  run_and_verify_tests
+
+  # Prompt user to accept deploy
+  prompt_user_to_complete_deploy
+end
+
+###
+# Helper method for:
+# 1. Checking whether any uncommited changes are present in git, and
+# 2. Raising an exception if so.
+def check_for_uncommited_changes
+  fail DeployError, 'Please commit changes before deploying.' if DeployCommands.git_status.present?
+end
+
+###
+# Helper method for:
+# 1. Running tests and outputting the results, and
+# 2. Raising an exception if any tests failed.
+def run_and_verify_tests
   # Run tests and output result
   test_result = DeployCommands.run_tests
   DeployCommands.output(test_result)
 
   # Fail deploy if any strings matching "(non-zero-number) failure" occurred
-  if test_result.match(TESTS_FAILED_REGEX) || !test_result.match(TESTS_PASSED_REGEX)
-    fail DeployError, 'Tests did not pass!'
-  end
+  fail DeployError, 'Tests did not pass!' if test_result.match(TESTS_FAILED_REGEX) || !test_result.match(TESTS_PASSED_REGEX)
+end
 
+###
+# Helper method for:
+# 1. Prompting the user to complete the deploy, and
+#      a. Completing the deploy if the user accepts, or
+#      b. Raising an exception if the user declines.
+def prompt_user_to_complete_deploy
   # Prompt user to accept deploy
-  DeployCommands.output('Please review the tests results above.')
-  DeployCommands.output("If they are acceptable, type #{ACCEPT_DEPLOY_TEXT} to start the deploy.")
+  DeployCommands.output("Please review any output above, then type #{ACCEPT_DEPLOY_TEXT} to start the deploy.")
 
   # If the user accepts the deploy, deploy.
   # Otherwise, abort.
-  if STDIN.gets.strip == ACCEPT_DEPLOY_TEXT
+  if DeployCommands.input == ACCEPT_DEPLOY_TEXT
     DeployCommands.run_deploy
   else
     fail DeployError, 'User declined deploy!'
   end
 end
-
-# Error class for exceptions that occur during a deploy attempt
-class DeployError < StandardError; end
 
 ###
 # Wrapper for various method calls.
@@ -34,6 +61,14 @@ class DeployError < StandardError; end
 class DeployCommands
   def self.output(message)
     puts message
+  end
+
+  def self.input
+    STDIN.gets.strip
+  end
+
+  def self.git_status
+    `git status -s`
   end
 
   def self.run_tests
--- spec/tasks/deploy_task_spec.rb
+++ spec/tasks/deploy_task_spec.rb
@@ -8,14 +8,14 @@ describe 'deploy task' do
   end
 
   before do
-    # Stub run_deploy call
-    # We don't want to actually have the method run, as doing so would trigger a deploy.
+    # Stub all DeployCommands calls
+    # These are also stubbed later on, but we need to be 100% sure that these are never actually run.
+    # Doing so could have unintended consequences (e.g. actually triggering a deploy).
+    allow(DeployCommands).to receive(:output) # Actual output will interfere with deploys
+    allow(DeployCommands).to receive(:input)
+    allow(DeployCommands).to receive(:git_status)
+    allow(DeployCommands).to receive(:run_tests)
     allow(DeployCommands).to receive(:run_deploy)
-
-    # Stub puts call
-    # There's no need to actually puts here - doing so could interfere with actual deploys.
-    # (e.g. as output from "when tests fail" tests could lead to a deploy being aborted)
-    allow(DeployCommands).to receive(:output)
   end
 
   # Helper method to run the task
@@ -24,71 +24,110 @@ describe 'deploy task' do
     Rake.application.invoke_task('deploy')
   end
 
-  context 'when tests fail' do
-    ['2392 examples, 111 failures', '35 examples, 10 failures', '20 examples, 1 failure', 'WeirdSample'].each do |failure_result_string|
-      it "should abort for #{failure_result_string}" do
-        # Stub run_tests to return the relevant failure result string
-        allow(DeployCommands).to receive(:run_tests).and_return(failure_result_string)
+  context "when uncommited changes are present" do
+    before do
+      # Stub check_for_uncommited_changes to return uncommited changes
+      allow(DeployCommands).to receive(:git_status).and_return("M lib/tasks/deploy.rake")
+    end
 
-        # Run task and make sure it raises a DeployError
-        expect do
-          run_rake_task
-        end.to raise_error(DeployError, 'Tests did not pass!')
+    it "should fail the deploy" do
+      # Run task and make sure it raises a DeployError
+      expect do
+        run_rake_task
+      end.to raise_error(DeployError, 'Please commit changes before deploying.')
 
-        # Make sure that
-        # 1. DeployCommands.run_tests was called,
-        # 2. DeployCommands.run_deploy was not called
-        expect(DeployCommands).to have_received(:run_tests)
-        expect(DeployCommands).to_not have_received(:run_deploy)
-      end
+      # Make sure that
+      # 1. DeployCommands#check_for_uncommited_changes was called,
+      # 2. DeployCommands#run_tests was not called,
+      # 3. DeployCommands#input was not called,
+      # 4. DeployCommands#run_deploy was not called
+      expect(DeployCommands).to have_received(:git_status)
+      expect(DeployCommands).to_not have_received(:run_tests)
+      expect(DeployCommands).to_not have_received(:input)
+      expect(DeployCommands).to_not have_received(:run_deploy)
     end
   end
 
-  context 'when tests pass' do
+  context "when no uncommited changes are present" do
     before do
-      # Stub run_tests to return a success
-      allow(DeployCommands).to receive(:run_tests).and_return('35 examples, 0 failures')
+      # Stub check_for_uncommited_changes to return no uncommited changes
+      allow(DeployCommands).to receive(:git_status).and_return("")
     end
 
-    context 'when the user declines the deploy' do
-      %w(deploy decline DECLINE).each do |decline_string|
-        it "should abort for #{decline_string}" do
-          # Stub STDIN.gets to return the decline string
-          allow(STDIN).to receive(:gets).and_return(decline_string)
+    context 'when tests fail' do
+      ['2392 examples, 111 failures', '35 examples, 10 failures', '20 examples, 1 failure', 'WeirdSample'].each do |failure_result_string|
+        it "should abort for #{failure_result_string}" do
+          # Stub run_tests to return the relevant failure result string
+          allow(DeployCommands).to receive(:run_tests).and_return(failure_result_string)
 
           # Run task and make sure it raises a DeployError
           expect do
             run_rake_task
-          end.to raise_error(DeployError, 'User declined deploy!')
+          end.to raise_error(DeployError, 'Tests did not pass!')
 
           # Make sure that
-          # 1. DeployCommands.run_tests was called,
-          # 2. STDIN.gets was called,
-          # 3. DeployCommands.run_deploy was not called
+          # 1. DeployCommands#check_for_uncommited_changes was called,
+          # 2. DeployCommands#run_tests was called,
+          # 3. DeployCommands#input was not called,
+          # 4. DeployCommands#run_deploy was not called
+          expect(DeployCommands).to have_received(:git_status)
           expect(DeployCommands).to have_received(:run_tests)
-          expect(STDIN).to have_received(:gets)
+          expect(DeployCommands).to_not have_received(:input)
           expect(DeployCommands).to_not have_received(:run_deploy)
         end
       end
     end
 
-    context 'when the user accepts the deploy' do
+    context 'when tests pass' do
       before do
-        # Stub STDIN.gets to return the accept string
-        allow(STDIN).to receive(:gets).and_return('DEPLOY')
+        # Stub run_tests to return a success
+        allow(DeployCommands).to receive(:run_tests).and_return('35 examples, 0 failures')
       end
 
-      it 'should deploy' do
-        # Run task
-        run_rake_task
+      context 'when the user declines the deploy' do
+        %w(deploy decline DECLINE).each do |decline_string|
+          it "should abort for #{decline_string}" do
+            # Stub DeployCommands#input to return the decline string
+            allow(DeployCommands).to receive(:input).and_return(decline_string)
 
-        # Make sure that
-        # 1. DeployCommands.run_tests was called,
-        # 2. STDIN.gets was called,
-        # 3. DeployCommands.run_deploy was called
-        expect(DeployCommands).to have_received(:run_tests)
-        expect(STDIN).to have_received(:gets)
-        expect(DeployCommands).to have_received(:run_deploy)
+            # Run task and make sure it raises a DeployError
+            expect do
+              run_rake_task
+            end.to raise_error(DeployError, 'User declined deploy!')
+
+            # Make sure that
+            # 1. DeployCommands#check_for_uncommited_changes was called,
+            # 2. DeployCommands#run_tests was called,
+            # 3. DeployCommands#input was called,
+            # 4. DeployCommands#run_deploy was not called
+            expect(DeployCommands).to have_received(:git_status)
+            expect(DeployCommands).to have_received(:run_tests)
+            expect(DeployCommands).to have_received(:input)
+            expect(DeployCommands).to_not have_received(:run_deploy)
+          end
+        end
+      end
+
+      context 'when the user accepts the deploy' do
+        before do
+          # Stub DeployCommands#input to return the accept string
+          allow(DeployCommands).to receive(:input).and_return('DEPLOY')
+        end
+
+        it 'should deploy' do
+          # Run task
+          run_rake_task
+
+          # Make sure that
+          # 1. DeployCommands#check_for_uncommited_changes was called,
+          # 2. DeployCommands#run_tests was called,
+          # 3. DeployCommands#input was called,
+          # 4. DeployCommands#run_deploy was called
+          expect(DeployCommands).to have_received(:git_status)
+          expect(DeployCommands).to have_received(:run_tests)
+          expect(DeployCommands).to have_received(:input)
+          expect(DeployCommands).to have_received(:run_deploy)
+        end
       end
     end
   end
